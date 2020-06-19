20e37f8cefc98b3353b4b5b60c38b771f83ff4db
xtrp <36894883+xtrp@users.noreply.github.com>
2020-06-18 23:19:21 -0400

    Basic refactoring for clarity, maintainability, and scalability

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const childProcess = require('child_process');
 
-const GET_LATEST_COMMAND = 'git log --abbrev-commit HEAD -1';
-const GET_ALL_COMMAND = 'git log --abbrev-commit HEAD';
+const GIT_COMMANDS = {
+	latestCommit: 'git log --abbrev-commit HEAD -1',
+	allCommits: 'git log --abbrev-commit HEAD',
+};
 
-const findCommitIDs = (command) => {
+const ERROR_MESSAGES = {
+	getLatestCommitFailed: 'Internal error: could not get latest commit unique ID from Git. Try checking your Git installation.',
+};
+
+const getCommitIDsFromCommand = (command) => {
 	const out = childProcess.execSync(command).toString();
 	const outLines = out.split('\n');
 
@@ -13,7 +19,7 @@ const findCommitIDs = (command) => {
 
 	outLines.forEach((line) => {
 		if(line.startsWith(COMMIT_KEYWORD)) {
-			const lineWords = line.split(" ");
+			const lineWords = line.split(' ');
 			const lineCommitID = lineWords[ lineWords.indexOf(COMMIT_KEYWORD) + 1 ];
 			commitIDs.push(lineCommitID);
 		}
@@ -24,15 +30,15 @@ const findCommitIDs = (command) => {
 
 module.exports = {
 	latest: () => {
-		const latestCommitInArr = findCommitIDs(GET_LATEST_COMMAND);
+		const latestCommitInArr = getCommitIDsFromCommand(GIT_COMMANDS[latestCommit]);
 		
 		if(latestCommitInArr.length < 1) {
-			throw new Error('Internal error: could not get latest commit unique ID from Git. Try checking your Git installation.');
+			throw new Error(ERROR_MESSAGES[getLatestCommitFailed]);
 		}
 
 		return latestCommitInArr[0];
 	},
 	all: () => {
-		return findCommitIDs(GET_ALL_COMMAND);
+		return getCommitIDsFromCommand(GIT_COMMANDS[allCommits]);
 	}
 }
